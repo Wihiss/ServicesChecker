f151103b5aa1b9bc4494a8fcd8cb36c3147b4551
Wihiss <wihiss@gmail.com>
2019-11-18 21:29:33 -0500

    [*] Switch to .Net Core 3.0 [*] New Dockerfiles with minimization of images size [*] Updated script.txt (versioning of images, docker push commands)

--- ServiceCheckerWebApp/Startup.cs
+++ ServiceCheckerWebApp/Startup.cs
@@ -37,7 +37,9 @@ namespace ServiceCheckerWebApp
         {
             services.AddSingleton(_historyLoader);
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+
+            services.AddMvc(option => option.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
